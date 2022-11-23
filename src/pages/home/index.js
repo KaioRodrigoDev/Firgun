7bc0a953e59581c5564a0834448ddb416c6c9335
KaioRodrigoDev <kaiorodrigocoder@gmail.com>
2022-11-23 10:57:45 -0300

    Adjusts

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,5 @@
 import Form from '../../components/forms'
 import './App.css'
-import { Button } from 'react-bootstrap'
 
 function Home() {
   return (
@@ -8,18 +7,6 @@ function Home() {
       <header className="App-header">
         <p>Cadastre-se de Leads</p>
         <Form />
-        <a
-          className="App-link"
-          href={localStorage.getItem('token') === null ? 'login' : 'dashboard'}
-          rel="noopener noreferrer"
-        >
-          Visualizar Cadastros
-        </a>
-        <div className="d-flex">
-          <Button variant="primary" type="submit">
-            Salvar
-          </Button>
-        </div>
       </header>
     </div>
   )
